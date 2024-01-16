d3e44a568ef51298a51bd810bb2ba9a469819e1c
Amir Kiarash Abbasi <61062258+keyarashabbasi@users.noreply.github.com>
2024-01-15 20:50:53 -0500

    Add files via upload

---
+++ ex1_test.py
@@ -0,0 +1,5 @@
+import get_a_point_for_writing_your_name as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+
---
+++ get_a_point_for_writing_your_name.py
@@ -0,0 +1,2 @@
+def hi_my_name_is():
+    return "Seyed Amir Kiarash Abbasi" # Write your name inside the quotation marks
